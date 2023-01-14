d67f13e0f0fa196eb8fc291a52fa23e39edd6d6d
TheMVProgrammer <franklin0415@hotmail.com>
2023-01-14 17:29:05 -0400

    Finishing Error and Loader components. Fetching API using redux.

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders:(headers) => {
+            headers.set('X-RapidAPI-Key', '2983f3cb21mshd4dbfbcb763706ap1907eejsn3c84c3a0f699');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
